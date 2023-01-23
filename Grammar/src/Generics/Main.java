d1442fecd21606daf3fbf06bd84d27df6237c090
SANGWOO-JOO <turncoat19@gmail.com>
2023-01-23 23:52:39 +0900

    Generic

--- Grammar/src/Generics/Main.java
+++ Grammar/src/Generics/Main.java
@@ -14,10 +14,26 @@ import java.util.ArrayList;
     }
 }
 
+class GenericClass2<T ,E>{
+      T ob;
+      E ob2;
+
+    public GenericClass2(T ob, E ob2) {
+        this.ob = ob;
+        this.ob2 = ob2;
+    }
+
+    void print(){
+        System.out.println(ob.getClass().getName());
+    }
+}
+
   public class Main {
     public static void main(String[] args) {
 
         GenericClass<Integer> generic =new GenericClass<>(10);
+        GenericClass2<Integer, Integer> generic2 =new GenericClass2<>(2,10);
         generic.printType();
+        generic2.print();
     }
 }
