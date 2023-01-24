28bd1b1161d81b2e15e47eba5a9acd1328a1da83
SANGWOO-JOO <turncoat19@gmail.com>
2023-01-24 23:48:24 +0900

    study

---
+++ Grammar/src/Stream/Practice/Main2.java
@@ -0,0 +1,37 @@
+package Stream.Practice;
+@FunctionalInterface
+interface MyFunction{
+    void run();
+}
+
+class Ex{
+    static void execute(MyFunction f){
+        f.run();
+    }
+
+    static MyFunction getMyFunction(){
+        MyFunction f =() -> System.out.println("f3.fun()");
+        return f;
+    }
+}
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        MyFunction f1 = () -> System.out.println("f1.fun()");
+
+        MyFunction f2 = new MyFunction() {
+            @Override
+            public void run() {
+                System.out.println("f2.fun()");
+            }
+        };
+
+        MyFunction f3 = Ex.getMyFunction();
+
+        f1.run();
+        f2.run();
+        f3.run();
+        
+    }
+}
