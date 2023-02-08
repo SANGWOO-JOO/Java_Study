eca1e77e4750f046ddc4b350c0297da31d49051a
SANGWOO-JOO <turncoat19@gmail.com>
2023-02-08 23:44:00 +0900

    study

--- Grammar/src/Calcurator/Calcurator.java
+++ Grammar/src/Calcurator/Calcurator.java
@@ -4,47 +4,34 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calcurator {
-    public static void main(String[] args) {
-        //사칙 연산
-        Scanner scanner = new Scanner(System.in);
+    int result = 0;
 
-        String value = scanner.nextLine(); // 값(문자열) 입력
-        String[] values = value.split(" "); // 문자열 분리
+    int minus(int operator1, int operator2){
+        return result=operator1-operator2;
+    }
+    int plus(int operator1, int operator2){
+        return result=operator1+operator2;
+    }
+    int multiply(int operator1, int operator2){
+        return  result=operator1*operator2;
+    }
+    int divide(int operator1, int operator2){
+        return result=operator1/operator2;
+    }
 
-            int[] num = new int[value.length()];
-    //      int[] result = new int[value.length()];
+    int calcurator(int operator1 , String oper, int operator2){
 
-
-            int size = values.length;
-            int startnumber =Integer.parseInt(values[0]);
-            int finishnumber = 0;
-
-        for(int i=0 ; i<size;i++){
-
-            if(values[i].equals("+")){
-                finishnumber = Integer.parseInt(values[i+1]);
-                startnumber =startnumber+finishnumber; // 처음 숫자로
-                i++;
-            }
-            else if(values[i].equals("-")){
-                finishnumber = Integer.parseInt(values[i+1]);
-                startnumber =startnumber-finishnumber; // 처음 숫자로
-                i++;
-            }
-            else if(values[i].equals("/")){
-                finishnumber = Integer.parseInt(values[i+1]);
-                startnumber =startnumber/finishnumber; // 처음 숫자로
-                i++;
-            }
-            else if(values[i].equals("*")){
-                finishnumber = Integer.parseInt(values[i+1]);
-                startnumber =startnumber*finishnumber; // 처음 숫자로
-                i++;
-            }
-
-            }
-
-            System.out.println(startnumber);
-
+        switch (oper) {
+            case "+":
+                plus(operator1, operator2);
+            case "-":
+                minus(operator1, operator2);
+            case "*":
+                multiply(operator1, operator2);
+            case "/":
+                divide(operator1, operator2);
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
---
+++ Grammar/src/Calcurator/CalcuratorTest.java
@@ -0,0 +1,7 @@
+package Calcurator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcuratorTest {
+
+}
