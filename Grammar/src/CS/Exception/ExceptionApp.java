2fe7426e7be2192a8fcd4a19c0f0c078d31d5042
SANGWOO-JOO <turncoat19@gmail.com>
2023-03-02 23:11:36 +0900

    Exception_Study

--- Grammar/src/CS/Exception/ExceptionApp.java
+++ Grammar/src/CS/Exception/ExceptionApp.java
@@ -3,7 +3,6 @@ package CS.Exception;
 public class ExceptionApp {
     public static void main(String[] args) {
         System.out.println(1);
-
         int [] scores = {1,2,3};
 
 //        System.out.println(scores[3]);  // 예외 발생
@@ -25,17 +24,27 @@ public class ExceptionApp {
 //        }
 //        System.out.println(3);
 
+        System.out.println("--- ");
         try {
             System.out.println(2);
-            System.out.println(scores[3]);
+//            System.out.println(scores[3]);
             System.out.println(3);
             System.out.println(2/0);
             System.out.println(4);
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("False"); // 1
-        }catch (ArithmeticException e){
+
+        }
+        catch (ArithmeticException e){
+            System.out.println("False" + e.getMessage());
+            e.printStackTrace();  // 몇 번째 줄에서 예외가 발생 했는지 -> 예외 메시지 발생   /
+        }
+        catch (Exception e){ // 데이터 타입 Exception
             System.out.println("False");
         }
+//        catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println("False"); // 1
+//        }catch (ArithmeticException e){
+//            System.out.println("False");
+//        }
         System.out.println(5);
 
     }
